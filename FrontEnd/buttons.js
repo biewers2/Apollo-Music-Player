16a0d9e73224b65404102b6c8f5ff2e36eadbea9
Caleb A. Collar <49846301+Mindstormer-0@users.noreply.github.com>
2020-01-24 13:52:56 -0800

    Add files via upload
    fixed js files

---
+++ FrontEnd/buttons.js
@@ -0,0 +1,41 @@
+function togglePlaying()
+{
+  document.getElementById("pause").style.display = "inline";
+  document.getElementById("play").style.display = "none";
+
+  fetch('http://localhost:5000/play', {    method: 'POST',
+    mode: "cors"//,    //json payload
+    //body: JSON.stringify({
+        //"greeting": "Hello from the browser!"
+    //})
+    });
+    j.then(function(response) { //fask should have printed
+    return response.text();
+    }).then(function (text) {    console.log('POST response: ');    // Should be 'OK' if everything was successful
+    console.log(text);
+    });
+}
+
+function toggleStopped()
+{
+  document.getElementById("pause").style.display = "none";
+  document.getElementById("play").style.display = "inline";
+
+  fetch('http://localhost:5000/play', {    method: 'POST',
+    mode: "cors"//,    //json payload
+    //body: JSON.stringify({
+        //"greeting": "Hello from the browser!"
+    //})
+    });
+    j.then(function(response) { //fask should have printed
+    return response.text();
+    }).then(function (text) {    console.log('POST response: ');    // Should be 'OK' if everything was successful
+    console.log(text);
+    });
+}
+
+function shuffle()
+{
+  document.getElementById("shuffle").style.color ="orange"
+}
+
