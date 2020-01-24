b179ebe4a70635de45537fce586988d057038bd5
Caleb A. Collar <49846301+Mindstormer-0@users.noreply.github.com>
2020-01-24 14:44:24 -0600

    Update Apollo.py
    Combined 'forntendhookupv1' with apollo.py for merging with master.

--- BackEnd/Apollo.py
+++ BackEnd/Apollo.py
@@ -11,241 +11,245 @@
 / / /_       __\ \_\/ / /      / / /____\/ //_______/\__\//_______/\__\// / /____\/ /    
 \_\___\     /____/_/\/_/       \/_________/ \_______\/    \_______\/    \/_________/                                                                                    
 Music Player using MPD
-Backend pre-release. V0
 '''
-
-import socket # for socket 
-import sys 
 import musicpd
+import msvcrt
+import json
+import subprocess
+import os
+import time
+import requests
+import xml.etree.ElementTree  as ET
+from subprocess import Popen, CREATE_NEW_CONSOLE
+from flask import Flask, render_template, request, redirect, Response
+from flask_cors import CORS 
+from urllib.request import urlopen
 
-try: 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    print ("Socket successfully created")
-except socket.error as err: 
-    print ("socket creation failed with error %s") %(err) 
-  
-# default port for socket 
-port = 6600
-  
-try: 
-    host_ip = ("localhost")
-except socket.gaierror: 
-  
-    # this means could not resolve the host 
-    print ("there was an error resolving the host")
-    sys.exit() 
-  
-# connecting to the server 
-s.connect((host_ip, port)) 
-  
-print ("the socket has successfully connected")
+app = Flask(__name__)
+CORS(app)  #is cors causing disconnects?
+client = musicpd.MPDClient()
+desired_volume = 50
 
-#Functions below
+def AlbumArtGenerator(album,artist,size):
+	key = 'd1915a9d435d47526a61dc0210978583'
+	retVal = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=' + key +'&artist='  + artist + '&album=' + album + '&format=json'
 
-def get_volume(): #now works when a song is not playing
- 	return desired_volume
-'''	
-def set_volume(s): #testing functionality to change volume when song is not playing
-	if s >= 0 and s <= 100:
-		client.setvol(s)'''
-def set_volume(vol): #wont let desired_volume be higher than 100 or lower than 0
-	global desired_volume
+	r = requests.get(url = retVal) 
+	r = r.content
+	r = r.decode("UTF-8")
+	k = json.loads(r)
+
+	try:
+		if size == 'medium':
+			if k['album']['image'][1]['#text'] != '':
+				return k['album']['image'][1]['#text']
+			else:
+				return './images/Logo1.png'
+		if size == 'mega':
+			if k['album']['image'][4]['#text'] != '':
+				return k['album']['image'][4]['#text']
+			else:
+				return './images/Logo1.png'
+	except:
+		return './images/Logo1.png'
+
+#@app.route('/start', methods = ['POST'])     #####PROBABLY SHOULDN'T HAVE A ROUTE
+def startup_func():
+	global desired_volume,client
+	program = 'C:/mpd/mpd.exe' #make sure your path is correct
+	args = 'C:/mpd/mpd.conf' #make sure your path is correct
+	try:
+		client.connect()
+	except:
+		Popen([program, args],creationflags = CREATE_NEW_CONSOLE) #CREATE_NEW_CONSOLE MAY ONLY WORK IN WINDOWS
+		client.connect()
+	#time.sleep(1) #was this just here to format the menu?
+	#if 'volume' in client.status():
+	if client.status()['state'] == 'play':
+		desired_volume = int(client.status()['volume'])	
+	else:
+		client.rescan()
+		client.clear()
+		temp_list = client.listall() 
+		for songs in temp_list: #builds a queue with all songs. can also use a playlist    ######################################Add -1 AS DEFAULT ARGUEMENT TO return_all_songs_as_list
+			if 'file' in songs and songs['file'].endswith('.mp3'):
+				client.add(songs['file'])
+	#client.add("Frozen 2 (Original Motion Picture Soundtrack)")
+
+@app.route('/play', methods = ['POST'])
+def play_pause():
+	if client.status()['state'] != 'play':
+		client.play()
+		#if the server was in a 'stop' state, this check will give it a second to add 'volume' back to status which will allow
+		#set_volume to work and avoid misreadings of the volume level
+		if 'volume' not in client.status():
+			time.sleep(1)
+		#desired_volume = int(client.status()['volume'])   ###
+		#client.setvol(desired_volume)  this one or the one above?
+
+		set_volume(desired_volume) 
+	else:
+		client.pause(1)
+	return 'OK',200
+
+@app.route('/volume', methods = ['POST'])
+def set_volume(x = -1): #wont let desired_volume be higher than 100 or lower than 0. default variable lets me call the function from another function
+	global desired_volume	
+	
+	if x != -1:
+		vol = x
+	else:
+		req = request.get_json()
+		vol = int(req['volume'])	
+	
 	if vol >= 0 and vol <= 100:
 		desired_volume = vol
 	elif vol < 0:
 		desired_volume = 0
 	else:
 		desired_volume = 100
-		
 	client.setvol(desired_volume)
+	return 'OK',200
 
-def seek(s):
+@app.route('/seek', methods = ['POST'])
+def seek():
+	req = request.get_json()
+	pos = float(req['seek'])
+	
 	if 'duration' in client.status():
-		if s < float(client.status()['duration']):
-			client.seekcur(s) 
-		else: client.next()
-
-def list_songs(): #lists songs in the playlist
-	for song in client.playlistinfo():
-		temp_string = (song["file"]) 
-		temp_string = song_stripper(temp_string)
-		print(temp_string)
+		pos *= float(client.status()['duration'])
+		if pos < float(client.status()['duration']):			
+			pos = int(pos)
+			client.seekcur(pos)
+		else: next_song()   
+		
+	return 'OK',200
 '''
-#works and marked final, but testing another version of each function
+@app.route('/next', methods = ['POST'])
 def next_song():
-	if client.status()['state'] == 'play': #this check prevented a crash on my system that didn't happen on other peoples
+	if client.status()['state'] == 'play' and int(client.status()['song']) != int(client.status()['playlistlength']) -1:
 		client.next()
+	return 'OK',200
+@app.route('/previous', methods = ['POST'])
 def prev_song():
 	if client.status()['state'] == 'play': #this check prevented a crash on my system that didn't happen on other peoples
 		if float(client.status()['elapsed']) > 3.: #if the song has played for over 3 seconds, start it over. otherwise play the previous song
-			client.seekcur(0)  
-		else:
-			client.previous()
-'''
-
-
-def next_song():
-	if client.status()['state'] == 'play' and int(client.status()['song']) != int(client.status()['playlistlength']) -1: 
-		client.next()
-
-			
-			
-def prev_song():
-	if client.status()['state'] == 'play': #this check prevented a crash on my system that didn't happen on other peoples
-		if float(client.status()['elapsed']) > 3.: #if the song has played for over 3 seconds, start it over. otherwise play the previous song
-			client.seekcur(0)  
+			client.seekcur(0)
 		elif client.status()['song'] != '0':
 			client.previous()
-'''
-def play_pause():
-	if client.status()['state'] != 'play': 
-		client.play()
-	else:
-		client.pause(1)
-'''
-def play_pause():
-	if client.status()['state'] != 'play': 
-		client.play()
-		set_volume(desired_volume)
-		
-	else:
-		client.pause(1)
-''' 
-#works and marked final but testing another version
-def list_all_songs(): #will list only .mp3 files in the music directory, as well as in any directories inside 
-	for song in client.listall():
-		for x in song:
-			if song[x][-1] == '3':	#only shows .mp3 files (or any file extension that ends in '3')	
-				print(song[x])
-				#print(song_stripper(song[x])) instead of above line to strip out directory, numbering, and extension data
+	return 'OK',200
 '''
 
-def list_all_songs():
-	for path in client.listall():
-		for type in path:
-			if path[type].endswith('.mp3'):
-				print(song_stripper(path[type]))
+@app.route('/next', methods = ['GET'])
+def next_song():
+	if client.status()['state'] == 'play' and int(client.status()['song']) != int(client.status()['playlistlength']) -1:
+		client.next()
+	return json.dumps(return_current_song())
 
+@app.route('/previous', methods = ['GET'])
+def prev_song():
+	if client.status()['state'] == 'play': #this check prevented a crash on my system that didn't happen on other peoples
+		if float(client.status()['elapsed']) > 3.: #if the song has played for over 3 seconds, start it over. otherwise play the previous song
+			client.seekcur(0)
+		elif client.status()['song'] != '0':
+			client.previous()
+	return json.dumps(return_current_song())
 
-def song_stripper(s): 
+def song_stripper(s):
 	#finds last slash in filename to remove directories
-	index_of_slash = s.rfind('/') 
+	index_of_slash = s.rfind('/')
 	if index_of_slash != -1:
 		temp_string = s[index_of_slash + 1:]
 	else:
 		temp_string = s
-
 	#finds '.' at the end of file names to remove filetypes
 	neg_size = len(temp_string) * -1
-	for e in range(-1,-5,-1): 
+	for e in range(-1,-5,-1):
 		if e < neg_size:
 			break
 		elif temp_string[e] == '.':
 			temp_string = temp_string[0:e]
 			break
-
 	#finds the first letter in the file name to remove track numbers
 	#can mess up file names of songs that start with a number or character
 	size = len(temp_string) - 1
-	for e in range(6): 
-		if e == size:  
+	for e in range(6):
+		if e == size:
 			break		
 		elif temp_string[e].isalpha():
 			temp_string = temp_string[e:]
 			break
-
 	return temp_string
 
+@app.route('/all_songs', methods = ['GET'])
+def return_database_songs_as_list():
+	listOfSongs = []
+	client.rescan()
+	list = client.listallinfo()
 
-def add_song_to_queue(filename):
+	for type in list:
+		if 'file' in type and type['file'].endswith('.mp3'):
+			song = {}
+			#if 'title' in type:
+				#song['title'] = type['title']
+			#else:
+			song['title'] = song_stripper(type['file'])
+
+			#if song['title'] == 'Let it go (test)':
+			
+			song['AlbumArtMedium'] = AlbumArtGenerator(type['album'],type['artist'],'medium')
+			song['AlbumArtMega'] = AlbumArtGenerator(type['album'],type['artist'],'mega')
+			if 'artist' in type:
+				song['artist'] = type['artist']
+
+		
+			if 'album' in type:
+				song['album'] = type['album']
+			if 'date' in type:
+				song['year'] = type['date']
+			if 'duration' in type:
+				song['duration'] = type['duration']
+			listOfSongs.append(song)
+
+	return json.dumps(listOfSongs)
+
+@app.route('/get_vol', methods = ['GET'])
+def get_volume(): #now works when a song is not playing	##
+	retVal = {'volume':desired_volume}
+	return json.dumps(retVal) 
+
+
+@app.route('/get_cur', methods = ['GET'])
+def return_current_song(): #will currently return an empty list if nothing is return from client.currentsong()
+	#song = client.currentsong()
+	#x = client.playlistinfo(0)[0]
+	attributes = ['file','artist','album','date','duration']
+	status = client.status()
+	song = client.currentsong() if status['state'] != 'stop' else client.playlistinfo(0)[0]
+	elapsed = status['elapsed'] if status['state'] != 'stop' else '0'
+	curr_song = songBuilder(song, attributes) if status['state'] != 'stop' else songBuilder(client.playlistinfo(0)[0], attributes)
+
 	try:
-		client.add(filename)
+		#client.next() ###################
+		curr_song['title'] = curr_song.pop('file')
+		curr_song['title'] = song_stripper(curr_song['title'])
+		#if curr_song['title'] == 'Let it go (test)':
+			#print()###############################
+		curr_song['duration'] = song['duration']
+		curr_song['elapsed'] = elapsed
+		curr_song['AlbumArtMedium'] = AlbumArtGenerator(song['album'],song['artist'],'medium')
+		curr_song['AlbumArtMega'] = AlbumArtGenerator(song['album'],song['artist'],'mega')
 	except:
-		print("Could not find the file: " + filename)
+		pass
+	return json.dumps(curr_song)
 
-
-def return_queue_songs_as_list():  # Returns a list of the playlist
-	retval = []
-	for song in client.playlist():
-		if song.endswith('.mp3'):
-			retval.append(song[6:])
-
-	return retval
-
-
-def remove_song_from_queue(fileName):  # get back to this
-        song_in_playlist = False
-        # check each song in the playlist to see if the filename matches what is in playlist
-        # playlistSongs = return_playList_songs_as_list()
-        # should it go with append or with the remove function because of the UI element
-        playListOfSongs = return_queue_songs_as_list()
-        pos = 0
-        for song in playListOfSongs:
-            print(pos)
-            print(fileName)
-            print(song)
-            if fileName == song:
-                song_in_playlist = True
-                break
-            pos = pos + 1
-                
-        if song_in_playlist:
-            try:
-                client.delete(pos)
-            except:
-                print("something went horribly wrong with the remove function")
-
-
-client = musicpd.MPDClient()       # create client object
-client.connect(host_ip , port)
-print(client.mpd_version) 
-client.command_list_ok_begin()       # start a command list
-client.update()                     
-client.status()                      
-client.stats()
-results = client.command_list_end() 
-print(results)
-
-
-desired_volume = 50
-#checks to see if mpd is playing and if it is, sets the client volume to the mpd volume 
-if 'volume' in client.status() and client.status()['volume'] != '-1': 
-	desired_volume = int(client.status()['volume'])
-set_volume(desired_volume)
-
-print()
-print("q for quit")
-print("gvol for get volume")
-print("svol for set volume")
-print("space for play or pause")
-print("n for next song")
-print("p for previous song")
-print("k for seeking (go to a specific time in the song)")
-print("lsp for listing songs in the playlist")
-print("lss for listing all songs in mpd")
-print("ss for song stripper (except you probably shouldn't call it)")
-
-user_input = "b"
-
-while user_input != "q":
-	if user_input == "gvol":
-		print(get_volume())
-	elif user_input =="svol":
-		desired_volume = int(input("\nWhat do you want to set the volume to?: "))
-		set_volume(desired_volume)
-	elif user_input == " ":
-		play_pause()
-	elif user_input == "n":
-		next_song()
-	elif user_input == "p":
-		prev_song()
-	elif user_input == "k":
-		position = float(input("What position do you want?"))
-		seek(position)
-	elif user_input == "lsp":
-		list_songs()
-	elif user_input == "lss":
-		list_all_songs()
-	elif user_input == "ss":
-		song_stripper()
-	user_input = input()
+def songBuilder(song, attributes): 
+	curr_song = {}
+	for x in attributes:
+		try:
+			curr_song[x] = song[x]
+		except:
+			continue	
+	return curr_song
+startup_func()
+#print(return_database_songs_as_list())
