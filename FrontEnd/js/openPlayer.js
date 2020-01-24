70d165ff2bb5e39378b0cf6cabe82ad026522b1f
SantaRodrig <37818200+SantaRodrig@users.noreply.github.com>
2020-01-24 14:23:45 -0600

    Update openPlayer.js

--- FrontEnd/js/openPlayer.js
+++ FrontEnd/js/openPlayer.js
@@ -28,3 +28,90 @@ function go2Artists(){
   }
 }
 
+function nextSong()
+    {
+        fetch('http://localhost:5000/next_song', {
+
+            method: 'GET',
+            mode: "cors"
+
+            //json payload
+            //body: JSON.stringify({
+                //"greeting": "Hello from the browser" //})
+            }).then(function(response) { //fask should have printed 
+				return response.text();
+			}).then(function (text) {
+
+				console.log('POST response: ');
+
+				// Should be 'OK' if everything was successful
+				console.log(text);
+		});
+    }
+
+function togglePlaying()
+{
+  document.getElementById("pause").style.display = "inline";
+  document.getElementById("play").style.display = "none";
+  
+  var j =fetch('http://localhost:5000/play_pause', {
+
+    method: 'POST',
+    mode: "cors"//,
+
+    //json payload
+    //body: JSON.stringify({
+        //"greeting": "Hello from the browser!"
+    //})
+    });
+    j.then(function(response) { //fask should have printed 
+    return response.text();
+    }).then(function (text) {
+
+    console.log('POST response: ');
+
+    // Should be 'OK' if everything was successful
+    console.log(text);
+    });
+}
+
+function prevSong()
+    {
+        fetch('http://localhost:5000/prev_song', {
+
+            method: 'GET',
+            mode: "cors"
+
+            //json payload
+            //body: JSON.stringify({
+                //"greeting": "Hello from the browser"
+                //})
+            }).then(function(response) { //fask should have printed 
+				return response.text();
+			}).then(function (text) {
+
+            console.log('GET response: ');
+			// Should be 'OK' if everything was successful
+			console.log(text);
+		});
+	}
+
+function toggleStopped()
+{
+    
+  document.getElementById("pause").style.display = "none";
+  document.getElementById("play").style.display = "inline";
+
+  fetch('http://localhost:5000/play_pause', {
+
+    method: 'POST',
+    mode: "cors"//,
+
+    //json payload
+    //body: JSON.stringify({
+        //"greeting": "Hello from the browser!"
+    //})
+    }).then(function(response) { //fask should have printed 
+    console.log(response);
+    });
+}
