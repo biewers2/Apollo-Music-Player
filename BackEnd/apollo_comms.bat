2b58a09557a6ea9be0b5d6fb43a0748fe5f5193e
Caleb A. Collar <49846301+Mindstormer-0@users.noreply.github.com>
2020-01-26 15:52:10 -0600

    Update apollo_comms.bat

--- BackEnd/apollo_comms.bat
+++ BackEnd/apollo_comms.bat
@@ -4,8 +4,17 @@ set FLASK_ENV=development
 
 set FLASK_APP=Apollo.py
 
-start cmd /k flask run
+start cmd /k flask run --with-threads
 
 TIMEOUT /T 4
 
-start cmd /k ping -t 127.0.0.1
+powershell -Command Test-NetConnection 127.0.0.1 -Port 5000
+
+:loop
+
+TIMEOUT /T 1
+
+powershell -Command Test-NetConnection 127.0.0.1 -Port 5000
+
+goto loop
+
