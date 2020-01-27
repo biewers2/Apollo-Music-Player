0ae676c0fc501c4d21d073b761383102eb8e74a1
samuelssantiago <ssantiago1@carthage.edu>
2020-01-27 14:17:06 -0600

    Add files via upload

--- FrontEnd/js/openPlayer.js
+++ FrontEnd/js/openPlayer.js
@@ -7,12 +7,12 @@ function boot(){
 }
 
 function openSearch() {
-  document.getElementById("myOverlay").style.display = "block";
-
-}
-
-function closeSearch() {
-  document.getElementById("myOverlay").style.display = "none";
+ if (document.getElementById("mainSearch").style.display = "none"){
+  document.getElementById("mainSearch").style.display = "block";
+  document.getElementById("mainPlaylists").style.display = "none";
+  document.getElementById("mainArtists").style.display = "none";
+  document.getElementById("mainAlbums").style.display = "none";
+ }
 }
 
 function go2Playlists(){
@@ -20,22 +20,23 @@ function go2Playlists(){
   document.getElementById("mainPlaylists").style.display = "block";
   document.getElementById("mainArtists").style.display = "none";
   document.getElementById("mainAlbums").style.display = "none";
+  document.getElementById("mainSearch").style.display = "none";
   }
 }
-
 function go2Albums(){
   if (document.getElementById("mainAlbums").style.display = "none"){
   document.getElementById("mainAlbums").style.display = "block";
   document.getElementById("mainPlaylists").style.display = "none"
   document.getElementById("mainArtists").style.display = "none"
+  document.getElementById("mainSearch").style.display = "none"
   }
 }
-
 function go2Artists(){
   if (document.getElementById("mainArtists").style.display = "none"){
   document.getElementById("mainArtists").style.display = "block";
   document.getElementById("mainPlaylists").style.display = "none";
   document.getElementById("mainAlbums").style.display = "none";
+  document.getElementById("mainSearch").style.display = "none";
   }
 }
 
