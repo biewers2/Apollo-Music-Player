b4c0f7b7fd464de860f28f5ac47f296a5e3c5f3c
abiewer <abiewer@carthage.edu>
2020-01-29 15:50:36 -0600

    progress on popup menu

--- FrontEnd/js/openPlayer.js
+++ FrontEnd/js/openPlayer.js
@@ -1,4 +1,4 @@
-function boot(){
+function boot() {
   fetchAllSongs();
   addAlbums();
   addPlaylists();
@@ -15,32 +15,32 @@ function closeSearch() {
   document.getElementById("myOverlay").style.display = "none";
 }
 
-function go2Playlists(){
-  if (document.getElementById("mainPlaylists").style.display = "none"){
-  document.getElementById("mainPlaylists").style.display = "block";
-  document.getElementById("mainArtists").style.display = "none";
-  document.getElementById("mainAlbums").style.display = "none";
+function go2Playlists() {
+  if (document.getElementById("mainPlaylists").style.display = "none") {
+    document.getElementById("mainPlaylists").style.display = "block";
+    document.getElementById("mainArtists").style.display = "none";
+    document.getElementById("mainAlbums").style.display = "none";
   }
 }
 
-function go2Albums(){
-  if (document.getElementById("mainAlbums").style.display = "none"){
-  document.getElementById("mainAlbums").style.display = "block";
-  document.getElementById("mainPlaylists").style.display = "none"
-  document.getElementById("mainArtists").style.display = "none"
+function go2Albums() {
+  if (document.getElementById("mainAlbums").style.display = "none") {
+    document.getElementById("mainAlbums").style.display = "block";
+    document.getElementById("mainPlaylists").style.display = "none"
+    document.getElementById("mainArtists").style.display = "none"
   }
 }
 
-function go2Artists(){
-  if (document.getElementById("mainArtists").style.display = "none"){
-  document.getElementById("mainArtists").style.display = "block";
-  document.getElementById("mainPlaylists").style.display = "none";
-  document.getElementById("mainAlbums").style.display = "none";
+function go2Artists() {
+  if (document.getElementById("mainArtists").style.display = "none") {
+    document.getElementById("mainArtists").style.display = "block";
+    document.getElementById("mainPlaylists").style.display = "none";
+    document.getElementById("mainAlbums").style.display = "none";
   }
 }
 
 async function fetchAllSongs() {
-  const response = await fetch('http://localhost:5000/all_songs', {method: 'GET', mode: 'cors'});
+  const response = await fetch('http://localhost:5000/all_songs', { method: 'GET', mode: 'cors' });
   const allSongs = await response.json();
   return allSongs;
   //Using "await" for all async functions instead of ".then()"
@@ -49,181 +49,187 @@ async function fetchAllSongs() {
 async function generateLibrary() {
   var library = await fetchAllSongs();
   var songList = document.getElementById("libraryBody");
-  for (var i = 0; i < library.length; i++)(function(i) {
+  for (var i = 0; i < library.length; i++)(function (i) {
     var song = document.createElement("tr");
-    song.id='tr'+i.toString();
+    song.id = 'tr' + i.toString();
     var songFile = library[i].file;
     song.onclick = async function () { await playSong(songFile) };
-    song.innerHTML = "< span class=\"popuptext\" id = \"myPopup\" display=\"none\"> Popup text...</span >";
+
+    var popupMenu = document.createElement("scan");
+    popupMenu.id = "popupMenu";
+    popupMenu.innerHTML="I'm a popup Menu!";
+    popupMenu.display = "none";
+    popupMenu.hidden = true;
+
+    song.append(popupMenu);
 
     song.addEventListener('contextmenu', function (e) {
       e.preventDefault();
-      document.getElementById("myPopup").display="block";
+      var popupBlock = document.getElementById("myPopup");
+      if (popupBlock.display == "none") {
+        popupBlock.display = "block";
+      }
+      else {
+        popupBlock.display = "none";
+      }
     });
 
-      var cell = document.createElement("td");
-      cell.id='td'+i.toString()+':0';
-      var songName = document.createTextNode(library[i].title);
-      if(songName.textContent=='none'){
-        songName = document.createTextNode(library[i].file);
-      }
-      cell.appendChild(songName);
-      song.appendChild(cell);
+    var cell = document.createElement("td");
+    cell.id = 'td' + i.toString() + ':0';
+    var songName = document.createTextNode(library[i].title);
+    if (songName.textContent == 'none') {
+      songName = document.createTextNode(library[i].file);
+    }
+    cell.appendChild(songName);
+    song.appendChild(cell);
 
-      cell = document.createElement("td");
-      cell.id = 'td' + i.toString() + ':1';
-      var artistName = document.createTextNode(library[i].artist);
-      cell.appendChild(artistName);
-      song.appendChild(cell);
-
-
-      cell = document.createElement("td");
-      cell.id = 'td' + i.toString() + ':2';
-      var albumName = document.createTextNode(library[i].album);
-      cell.appendChild(albumName);
-      song.appendChild(cell);
+    cell = document.createElement("td");
+    cell.id = 'td' + i.toString() + ':1';
+    var artistName = document.createTextNode(library[i].artist);
+    cell.appendChild(artistName);
+    song.appendChild(cell);
 
 
-      cell = document.createElement("td");
-      cell.id = 'td' + i.toString() + ':3';
-      songLength = library[i].duration;
-      minutes = (songLength/60);
-      minutes = minutes.toFixed(2);
-      var duration = document.createTextNode(minutes);
-      cell.appendChild(duration);
-      song.appendChild(cell);
+    cell = document.createElement("td");
+    cell.id = 'td' + i.toString() + ':2';
+    var albumName = document.createTextNode(library[i].album);
+    cell.appendChild(albumName);
+    song.appendChild(cell);
+
+
+    cell = document.createElement("td");
+    cell.id = 'td' + i.toString() + ':3';
+    songLength = library[i].duration;
+    minutes = (songLength / 60);
+    minutes = minutes.toFixed(2);
+    var duration = document.createTextNode(minutes);
+    cell.appendChild(duration);
+    song.appendChild(cell);
 
     songList.appendChild(song);
   }(i));
 }
 
-async function addAlbums() { 
+async function addAlbums() {
   var library = await fetchAllSongs();
-  for(var i = 0; i < library.length; i++){
-    if (library[i].albumArt == "./images/Logo1.png"){
+  for (var i = 0; i < library.length; i++) {
+    if (library[i].albumArt == "./images/Logo1.png") {
       continue;
     }
-    else 
-    {
+    else {
       var img = document.createElement('img');
-      img.setAttribute("src" , library[i].albumArt);
+      img.setAttribute("src", library[i].albumArt);
       img.classList.add("square");
       img.classList.add("albumType");
-      img.setAttribute("id" , library[i].album);
+      img.setAttribute("id", library[i].album);
       document.getElementById("mainAlbums").append(img);
     }
   }
   albumButtons();
 }
 
-async function albumButtons(){
+async function albumButtons() {
   var library = await fetchAllSongs();
-  for(var i = 0; i < library.length; i++){
-    if (library[i].albumArt == "./images/Logo1.png"){
+  for (var i = 0; i < library.length; i++) {
+    if (library[i].albumArt == "./images/Logo1.png") {
       continue;
     }
-    else 
-    {
-    document.getElementById(library[i].album).setAttribute('onclick' ,function changePlaying() {
-      document.getElementById('currentAlbum').src=library[i].albumArt;
-    });
+    else {
+      document.getElementById(library[i].album).setAttribute('onclick', function changePlaying() {
+        document.getElementById('currentAlbum').src = library[i].albumArt;
+      });
+    }
   }
 }
+
+function addPlaylists() {
+  var img = document.createElement('img');
+  img.src = "https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814051_1280.png"
+  img.classList.add("square");
+  document.getElementById("mainPlaylists").append(img);
 }
 
-function addPlaylists() { 
-      var img = document.createElement('img');
-      img.src = "https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814051_1280.png"
-      img.classList.add("square");
-      document.getElementById("mainPlaylists").append(img);
-}
-
-async function addArtists() { 
+async function addArtists() {
   var library = await fetchAllSongs();
   var lastArtist = null;
   var artistTable = document.createElement('table');
   artistTable.classList.add("artistDisplay");
-  for(var i = 0; i < library.length; i++){
-  
-      var row = document.createElement("tr");
+  for (var i = 0; i < library.length; i++) {
 
-      var artistName = document.createElement("td");
-      var artist = document.createTextNode(library[i].artist);
-      lastArtist = artist;
-      artistName.append(artist);
-      row.append(artistName);
-      artistTable.append(row);
+    var row = document.createElement("tr");
+
+    var artistName = document.createElement("td");
+    var artist = document.createTextNode(library[i].artist);
+    lastArtist = artist;
+    artistName.append(artist);
+    row.append(artistName);
+    artistTable.append(row);
   }
 
-    document.getElementById("mainArtists").append(artistTable);
+  document.getElementById("mainArtists").append(artistTable);
 }
 
-function togglePlaying()
-{
+function togglePlaying() {
   document.getElementById("pause").style.display = "inline";
   document.getElementById("play").style.display = "none";
-  
-  var j =fetch('http://localhost:5000/play', {method: 'POST', mode: 'cors'});
-    j.then(function(response) { //fask should have printed 
+
+  var j = fetch('http://localhost:5000/play', { method: 'POST', mode: 'cors' });
+  j.then(function (response) { //fask should have printed 
     return response.text();
-    }).then(function (text) {
+  }).then(function (text) {
     console.log('POST response: ');
     console.log(text);
   });
 }
 
-function toggleStopped()
-{
-    
+function toggleStopped() {
+
   document.getElementById("pause").style.display = "none";
   document.getElementById("play").style.display = "inline";
 
-  fetch('http://localhost:5000/play', {method: 'POST', mode: 'cors'}).then(function(response) {
+  fetch('http://localhost:5000/play', { method: 'POST', mode: 'cors' }).then(function (response) {
     console.log(response);
   });
 }
 
-function nextSong()
-{
-  fetch('http://localhost:5000/next', {method: 'GET', mode: 'cors'}).then(function(response) {
-  return response.text();
+function nextSong() {
+  fetch('http://localhost:5000/next', { method: 'GET', mode: 'cors' }).then(function (response) {
+    return response.text();
   }).then(function (text) {
-  console.log('POST response: ');
-  console.log(text);
+    console.log('POST response: ');
+    console.log(text);
   });
 }
 
-function prevSong()
-{
-  fetch('http://localhost:5000/previous', {method: 'GET', mode: 'cors'}).then(function(response) { 
-  return response.text();
+function prevSong() {
+  fetch('http://localhost:5000/previous', { method: 'GET', mode: 'cors' }).then(function (response) {
+    return response.text();
   }).then(function (text) {
     console.log('GET response: ');
     console.log(text);
   });
 }
 
-function SetVolume(val) 
-{
+function SetVolume(val) {
   var player = document.getElementById('vol-control');
   console.log('Before: ' + player.volume);
   player.volume = val / 100;
   console.log('After: ' + player.volume);
 
-  var asJSON = JSON.stringify({'volume':val});
+  var asJSON = JSON.stringify({ 'volume': val });
   console.log(asJSON)
 
   //POST
   fetch('http://localhost:5000/volume', {
-            method: 'POST',
-            mode: "cors",
-            body: asJSON,
-            headers:{
-                "Content-Type": 'application/json'
-            }
-  }).then(function(response){
+    method: 'POST',
+    mode: "cors",
+    body: asJSON,
+    headers: {
+      "Content-Type": 'application/json'
+    }
+  }).then(function (response) {
     return response.text();
-  }).then(function(text){
+  }).then(function (text) {
     console.log('POST reponse: ');
 
     console.log(text);
@@ -255,10 +261,10 @@ async function remove_from_playlist(songPos, playlist) {
   //POST
   const response = await fetch('http://localhost:5000/remove_from_playlist', {
     method: 'POST',
-    mode:'cors',
+    mode: 'cors',
     body: asJSON
   });
-  
+
   const text = await response.text();
 
   console.log("POST response: ");
@@ -266,14 +272,14 @@ async function remove_from_playlist(songPos, playlist) {
   console.log(text);
 }
 
-async function playSong(songName){
-  var songOBJ={"file":songName};
+async function playSong(songName) {
+  var songOBJ = { "file": songName };
   var asJSON = JSON.stringify(songOBJ);
 
   const response = await fetch('http://localhost:5000/play_song', {
     method: 'POST',
-    mode:'cors',
-    body:asJSON
+    mode: 'cors',
+    body: asJSON
   });
 
   const text = await response.text();
