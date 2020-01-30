97957a35123ade6f93eb83ef2dd141179bd386d3
vincatzero <43866869+vincatzero@users.noreply.github.com>
2020-01-29 19:15:41 -0600

    Update Apollo.py

--- BackEnd/Apollo.py
+++ BackEnd/Apollo.py
@@ -137,8 +137,7 @@ def info_obj_builder():
 				temp_artist = {}
 				temp_artist['name'] = 'none'
 				temp_artist['albums'] = 'none'
-				
-
+				artists_list.append(temp_artist)
 
 	return 	{'songs':songs,'albums':albums_list,'artists':artists_list}
 
